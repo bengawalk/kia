0200b18a036228ed7932613eeb20a6eb7a666ac9
chaitanya-deep <5744488+chaitanya-deep@users.noreply.github.com>
2023-01-18 09:32:14 +0000

    Deploying to gh-pages from @ bengawalk/kia@ad35e6c5f8261d112325fab3c8d3bbca603c9705 🚀

---
+++ assets/install.989078f0.js
@@ -0,0 +1,54 @@
+import{j as o,r as C,L as r}from"./index.c5257e5a.js";import{i as D,S as l}from"./i18n.b2275617.js";import{r as c}from"./index.ee7ae7c7.js";import{R as F}from"./react-markdown.6ac11af6.js";const B="/assets/install_android_1.1df71918.webp",A="/assets/install_android_2.db96f1b0.webp",e="/assets/install_ios_1.f83f7c7f.webp",t="/assets/install_ios_2.96a7ec7f.webp",s="/assets/install_ios_3.f47ff182.webp",a="/assets/install_desktop.e368c155.webp",n={en:`
+# Install app
+The application is a [Progressive Web App(PWA)](https://en.wikipedia.org/wiki/Progressive_web_app) and hence, can be installed and used on any mobile/desktop device.  
+On installation, it behaves like a native app accessible from the Home screen launcher. This gives a faster, and more convenient experience while always staying up-to-date.
+
+## Installation instructions
+### Android
+- Click on the menu icon beside the address bar
+- Select "Install App" or "Add to Home screen" and confirm
+
+<img src=${B} alt="" width="250px" />
+<img src=${A} alt="" width="250px" />
+
+### iOs
+- Open the site in Safari
+- Click the share icon at the bottom
+- Click on "Add to Home Screen"
+
+<img src=${e} alt="" width="250px" />
+<img src=${t} alt="" width="250px" />
+<img src=${s} alt="" width="250px" />
+
+### Chrome
+- Click the install icon on the address bar and confirm
+
+<img src=${a} alt="" width="750px" />
+`,kn:`
+# \u0C85\u0CAA\u0CCD\u0CB2\u0CBF\u0C95\u0CC6\u0CD5\u0CB6\u0CA8\u0CCD \u0C87\u0CA8\u0CCD\u0CB8\u0CCD\u0C9F\u0CBE\u0CB2\u0CCD \u0CAE\u0CBE\u0CA1\u0CBF
+\u0C85\u0CAA\u0CCD\u0CB2\u0CBF\u0C95\u0CC7\u0CB6\u0CA8\u0CCD \u0C92\u0C82\u0CA6\u0CC1 [\u0CAA\u0CCD\u0CB0\u0C97\u0CA4\u0CBF\u0CB6\u0CC0\u0CB2 \u0CB5\u0CC6\u0CAC\u0CCD \u0C85\u0CAA\u0CCD\u0CB2\u0CBF\u0C95\u0CC7\u0CB6\u0CA8\u0CCD(PWA)](https://en.wikipedia.org/wiki/Progressive_web_app) \u0CAE\u0CA4\u0CCD\u0CA4\u0CC1 \u0C86\u0CA6\u0CCD\u0CA6\u0CB0\u0CBF\u0C82\u0CA6, \u0CAF\u0CBE\u0CB5\u0CC1\u0CA6\u0CC7 \u0CAE\u0CCA\u0CAC\u0CC8\u0CB2\u0CCD/\u0CA1\u0CC6\u0CB8\u0CCD\u0C95\u0CCD\u200C\u0C9F\u0CBE\u0CAA\u0CCD \u0CB8\u0CBE\u0CA7\u0CA8\u0CA6\u0CB2\u0CCD\u0CB2\u0CBF \u0CB8\u0CCD\u0CA5\u0CBE\u0CAA\u0CBF\u0CB8\u0CAC\u0CB9\u0CC1\u0CA6\u0CC1 \u0CAE\u0CA4\u0CCD\u0CA4\u0CC1 \u0CAC\u0CB3\u0CB8\u0CAC\u0CB9\u0CC1\u0CA6\u0CC1.
+\u0C85\u0CA8\u0CC1\u0CB8\u0CCD\u0CA5\u0CBE\u0CAA\u0CA8\u0CC6\u0CAF \u0CB8\u0CAE\u0CAF\u0CA6\u0CB2\u0CCD\u0CB2\u0CBF, \u0C87\u0CA6\u0CC1 \u0CB9\u0CCB\u0CAE\u0CCD \u0CB8\u0CCD\u0C95\u0CCD\u0CB0\u0CC0\u0CA8\u0CCD \u0CB2\u0CBE\u0C82\u0C9A\u0CB0\u0CCD\u200C\u0CA8\u0CBF\u0C82\u0CA6 \u0CAA\u0CCD\u0CB0\u0CB5\u0CC7\u0CB6\u0CBF\u0CB8\u0CAC\u0CB9\u0CC1\u0CA6\u0CBE\u0CA6 \u0CB8\u0CCD\u0CA5\u0CB3\u0CC0\u0CAF \u0C85\u0CAA\u0CCD\u0CB2\u0CBF\u0C95\u0CC7\u0CB6\u0CA8\u0CCD\u200C\u0CA8\u0C82\u0CA4\u0CC6 \u0CB5\u0CB0\u0CCD\u0CA4\u0CBF\u0CB8\u0CC1\u0CA4\u0CCD\u0CA4\u0CA6\u0CC6. \u0CAF\u0CBE\u0CB5\u0CBE\u0C97\u0CB2\u0CC2 \u0CA8\u0CB5\u0CC0\u0C95\u0CC3\u0CA4\u0CB5\u0CBE\u0C97\u0CBF\u0CB0\u0CC1\u0CB5\u0CBE\u0C97 \u0C87\u0CA6\u0CC1 \u0CB5\u0CC7\u0C97\u0CB5\u0CBE\u0CA6 \u0CAE\u0CA4\u0CCD\u0CA4\u0CC1 \u0CB9\u0CC6\u0C9A\u0CCD\u0C9A\u0CC1 \u0C85\u0CA8\u0CC1\u0C95\u0CC2\u0CB2\u0C95\u0CB0 \u0C85\u0CA8\u0CC1\u0CAD\u0CB5\u0CB5\u0CA8\u0CCD\u0CA8\u0CC1 \u0CA8\u0CC0\u0CA1\u0CC1\u0CA4\u0CCD\u0CA4\u0CA6\u0CC6.
+
+## \u0C85\u0CA8\u0CC1\u0CB8\u0CCD\u0CA5\u0CBE\u0CAA\u0CA8\u0CBE \u0CB8\u0CC2\u0C9A\u0CA8\u0CC6\u0C97\u0CB3\u0CC1
+### \u0C86\u0C82\u0CA1\u0CCD\u0CB0\u0CBE\u0CAF\u0CCD\u0CA1\u0CCD
+- \u0CB5\u0CBF\u0CB3\u0CBE\u0CB8 \u0CAA\u0C9F\u0CCD\u0C9F\u0CBF\u0CAF \u0CAA\u0C95\u0CCD\u0C95\u0CA6\u0CB2\u0CCD\u0CB2\u0CBF\u0CB0\u0CC1\u0CB5 \u0CAE\u0CC6\u0CA8\u0CC1 \u0C90\u0C95\u0CBE\u0CA8\u0CCD \u0CAE\u0CC7\u0CB2\u0CC6 \u0C95\u0CCD\u0CB2\u0CBF\u0C95\u0CCD \u0CAE\u0CBE\u0CA1\u0CBF
+- "\u0CAE\u0CC1\u0C96\u0CAA\u0CC1\u0C9F \u0CAA\u0CB0\u0CA6\u0CC6\u0C97\u0CC6 \u0CB8\u0CC7\u0CB0\u0CBF\u0CB8\u0CC1" \u0C86\u0CAF\u0CCD\u0C95\u0CC6\u0CAE\u0CBE\u0CA1\u0CBF \u0CAE\u0CA4\u0CCD\u0CA4\u0CC1 \u0CA6\u0CC3\u0CA2\u0CC0\u0C95\u0CB0\u0CBF\u0CB8\u0CBF
+
+<img src=${B} alt="" width="250px" />
+<img src=${A} alt="" width="250px" />
+
+### \u0C90\u0C92\u0C8E\u0CB8\u0CCD
+- \u0CB8\u0CAB\u0CBE\u0CB0\u0CBF\u0CAF\u0CB2\u0CCD\u0CB2\u0CBF \u0CB8\u0CC8\u0C9F\u0CCD \u0CA4\u0CC6\u0CB0\u0CC6\u0CAF\u0CBF\u0CB0\u0CBF
+- \u0C95\u0CC6\u0CB3\u0CAD\u0CBE\u0C97\u0CA6\u0CB2\u0CCD\u0CB2\u0CBF\u0CB0\u0CC1\u0CB5 \u0CB9\u0C82\u0C9A\u0CBF\u0C95\u0CC6 \u0C90\u0C95\u0CBE\u0CA8\u0CCD \u0C95\u0CCD\u0CB2\u0CBF\u0C95\u0CCD \u0CAE\u0CBE\u0CA1\u0CBF
+- "\u0CB9\u0CCB\u0CAE\u0CCD \u0CB8\u0CCD\u0C95\u0CCD\u0CB0\u0CC0\u0CA8\u0CCD\u200C\u0C97\u0CC6 \u0CB8\u0CC7\u0CB0\u0CBF\u0CB8\u0CC1" \u0C95\u0CCD\u0CB2\u0CBF\u0C95\u0CCD \u0CAE\u0CBE\u0CA1\u0CBF
+
+<img src=${e} alt="" width="250px" />
+<img src=${t} alt="" width="250px" />
+<img src=${s} alt="" width="250px" />
+
+### \u0C95\u0CCD\u0CB0\u0CCB\u0CAE\u0CCD
+- \u0CB5\u0CBF\u0CB3\u0CBE\u0CB8 \u0CAA\u0C9F\u0CCD\u0C9F\u0CBF\u0CAF\u0CB2\u0CCD\u0CB2\u0CBF\u0CB0\u0CC1\u0CB5 \u0C87\u0CA8\u0CCD\u200C\u0CB8\u0CCD\u0C9F\u0CBE\u0CB2\u0CCD \u0C90\u0C95\u0CBE\u0CA8\u0CCD \u0C95\u0CCD\u0CB2\u0CBF\u0C95\u0CCD \u0CAE\u0CBE\u0CA1\u0CBF \u0CAE\u0CA4\u0CCD\u0CA4\u0CC1 \u0CA6\u0CC3\u0CA2\u0CC0\u0C95\u0CB0\u0CBF\u0CB8\u0CBF
+
+<img src=${a} alt="" width="750px" />
+`},E=()=>{const[u,i]=C.exports.useState(localStorage.getItem("lang")||r[0].code);return C.exports.useEffect(()=>{localStorage.setItem("lang",u),document.documentElement.setAttribute("lang",u),D.changeLanguage(u)},[u]),C.exports.createElement(C.exports.Fragment,null,C.exports.createElement(l,{setLang:i}),C.exports.createElement(F,{rehypePlugins:[c],children:n[u]||n.en}))},p=o(document.getElementById("root"));p.render(C.exports.createElement(E,null));
+//# sourceMappingURL=install.989078f0.js.map
