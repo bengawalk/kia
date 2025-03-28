addf1c1fa24de5896dde9e3f3ffb5b6f98a8443b
seadeep42 <5744488+seadeep42@users.noreply.github.com>
2025-03-28 06:42:13 +0000

    Deploying to gh-pages from @ bengawalk/kia@97849284854aad7524e3593d971761476cffa72c 🚀

--- assets/help-support-b5c7b330.js
+++ assets/help-support-b5c7b330.js
@@ -1,4 +1,4 @@
-import{q as s,r as t,o as a,L as r}from"./index-08f43efd.js";import{i,S as m}from"./i18n-363a27bc.js";import{M as l}from"./index.modern-de643964.js";const n={en:`
+import{q as s,r as t,o as a,L as r}from"./index-29df1238.js";import{i,S as m}from"./i18n-fb184d55.js";import{M as l}from"./index.modern-5a330297.js";const n={en:`
 # Help and Support
 ## Immediate help
 For information on bus status, next available bus, timings, and issues with current bus please reach out to the BMTC Vayu Vajra helpline at [77609 91269](tel:7760991269)
@@ -17,4 +17,4 @@ You can also find us on [Twitter](https://twitter.com/bengawalk), [Instagram](ht
 ಸೈಟ್ ಅನ್ನು ಟೀಮ್ ಬೆಂಗಾವಾಕ್ ವಿನ್ಯಾಸಗೊಳಿಸಿದೆ ಮತ್ತು ಅಭಿವೃದ್ಧಿಪಡಿಸಿದೆ. ಅಪ್ಲಿಕೇಶನ್‌ಗಾಗಿ ದೋಷಗಳು, ಸಮಸ್ಯೆಗಳು ಅಥವಾ ವೈಶಿಷ್ಟ್ಯದ ವಿನಂತಿಗಳ ಸಂದರ್ಭದಲ್ಲಿ, [ಡಿಸ್ಕಾರ್ಡ್](https://discord.gg/XhmvDP4kXp) ಅಥವಾ [ಗಿಥಬ್‌ನಲ್ಲಿ](https://github.com/bengawalk/kia) ನಮ್ಮನ್ನು ಸಂಪರ್ಕಿಸಿ.  
 ನೀವು ನಮ್ಮನ್ನು [ಟ್ವಿಟರ್](https://twitter.com/bengawalk), [ಇನ್ಸ್ಟಾಗ್ರಾಮ್](https://www.instagram.com/bengawalk/) ಮತ್ತು [ಯೂಟ್ಯೂಬ್](https://www.youtube.com/@bengawalk) ನಲ್ಲಿಯೂ ಕಾಣಬಹುದು.
 `},u=()=>{const[e,o]=t.useState(a.getItem("lang")||r[0].code);return t.useEffect(()=>{a.setItem("lang",e),document.documentElement.setAttribute("lang",e),i.changeLanguage(e)},[e]),t.createElement(t.Fragment,null,t.createElement(m,{setLang:o}),t.createElement(l,null,n[e]||n.en))},c=s(document.getElementById("root"));c.render(t.createElement(u,null));
-//# sourceMappingURL=help-support-9dde0073.js.map
+//# sourceMappingURL=help-support-b5c7b330.js.map
--- assets/how-to-use-5de8cdd4.js
+++ assets/how-to-use-5de8cdd4.js
@@ -1,4 +1,4 @@
-import{q as l,r as e,o as a,L as c}from"./index-08f43efd.js";import{i as h,S as m}from"./i18n-363a27bc.js";import{M as d}from"./index.modern-de643964.js";const o="/assets/howto_1-830f2426.webp",n="/assets/howto_2-7abb96fc.webp",s="/assets/howto_3-48b91a21.webp",i={en:`
+import{q as l,r as e,o as a,L as c}from"./index-29df1238.js";import{i as h,S as m}from"./i18n-fb184d55.js";import{M as d}from"./index.modern-5a330297.js";const o="/assets/howto_1-830f2426.webp",n="/assets/howto_2-7abb96fc.webp",s="/assets/howto_3-48b91a21.webp",i={en:`
 # How to use
 If the user gives permission for location, the application automatically chooses the best available bus to reach the airport.  
 If not, you could manually find the closest bus and all relevant details in 3 simple steps
@@ -40,4 +40,4 @@ The fare ranges from ₹200 to ₹350 based on the boarding point and distance t
 ### ದರದ ವಿವರಗಳು
 ಬೋರ್ಡಿಂಗ್ ಪಾಯಿಂಟ್ ಮತ್ತು ವಿಮಾನ ನಿಲ್ದಾಣದ ದೂರವನ್ನು ಆಧರಿಸಿ ದರವು ₹200 ರಿಂದ ₹350 ರಷ್ಟಿರುತ್ತದೆ.
 `},g=()=>{const[t,r]=e.useState(a.getItem("lang")||c[0].code);return e.useEffect(()=>{a.setItem("lang",t),document.documentElement.setAttribute("lang",t),h.changeLanguage(t)},[t]),e.createElement(e.Fragment,null,e.createElement(m,{setLang:r}),e.createElement(d,null,i[t]||i.en))},p=l(document.getElementById("root"));p.render(e.createElement(g,null));
-//# sourceMappingURL=how-to-use-ae7eb7aa.js.map
+//# sourceMappingURL=how-to-use-5de8cdd4.js.map
