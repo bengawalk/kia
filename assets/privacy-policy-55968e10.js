34914931855aa3db3a21057b35519289be3f0799
seadeep42 <5744488+seadeep42@users.noreply.github.com>
2025-04-01 03:20:04 +0000

    Deploying to gh-pages from @ bengawalk/kia@4081958c77ef90888c66952b1b0c5f1e63b5b128 🚀

--- assets/privacy-policy-55968e10.js
+++ assets/privacy-policy-55968e10.js
@@ -1,4 +1,4 @@
-import{q as s,r as e,o,L as n}from"./index-29df1238.js";import{i as r,S as c}from"./i18n-fb184d55.js";import{M as l}from"./index.modern-5a330297.js";const a={en:`
+import{q as s,r as e,o,L as n}from"./index-c987b794.js";import{i as r,S as c}from"./i18n-e38b007d.js";import{M as l}from"./index.modern-a9a693bd.js";const a={en:`
 # Privacy policy
 The purpose of this privacy policy is to be transparent and give an accurate detail of how the website handles the data you provide while using it.
 
@@ -91,4 +91,4 @@ For any clarification and information regarding privacy, please reach out to [de
 ### ಸಂಪರ್ಕಿಸಿ
 ಗೌಪ್ಯತೆಗೆ ಸಂಬಂಧಿಸಿದಂತೆ ಯಾವುದೇ ಸ್ಪಷ್ಟೀಕರಣ ಮತ್ತು ಮಾಹಿತಿಗಾಗಿ, ದಯವಿಟ್ಟು [deep@euclidxr.com](mailto:deep@euclidxr.com) ಅನ್ನು ಸಂಪರ್ಕಿಸಿ
 `},d=()=>{const[t,i]=e.useState(o.getItem("lang")||n[0].code);return e.useEffect(()=>{o.setItem("lang",t),document.documentElement.setAttribute("lang",t),r.changeLanguage(t)},[t]),e.createElement(e.Fragment,null,e.createElement(c,{setLang:i}),e.createElement(l,null,a[t]||a.en))},h=s(document.getElementById("root"));h.render(e.createElement(d,null));
-//# sourceMappingURL=privacy-policy-94fc4d4d.js.map
+//# sourceMappingURL=privacy-policy-55968e10.js.map
